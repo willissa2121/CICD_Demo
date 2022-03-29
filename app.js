8c1c50a7e45169cdabfb6858117f4f66063e2e67
willissa2121 <you@example.com>
2022-03-29 14:19:44 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const doubleFunc = (integer) => {
+  return integer * 2;
+};
+
+module.exports = { doubleFunc };
---
+++ app.spec.js
@@ -0,0 +1,5 @@
+const { doubleFunc } = require('./app');
+
+test('doubles the value 2', () => {
+  expect(doubleFunc(2)).toBe(4);
+});
